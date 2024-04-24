59492969296696d0cdd5f11e94436a189c1d155b
nitzan201 <segall.nitzan@gmail.com>
2024-04-24 13:00:44 +0300

    add 0dev

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { WorkoutCard } from "@/components/component/workout-card";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      Home <Button variant={"outline"}></Button>
+    <div className="my-8 relative w-fit">
+      <WorkoutCard />
     </div>
   );
 };
